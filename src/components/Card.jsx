dac5a1e099600fb281f60e9e2bd69398a57134b7
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-16 17:21:08 -0300

    Add openSpots prop to Card and display badgeText

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,33 @@
-import star from '../assets/star.png'
+import star from "../assets/star.png";
 
 function Card(props) {
-    return(
-        <div className="card">
-            <img src={`/src/assets/${props.img}`} className="card--image" alt={props.alt} />
-            <div className="card--stats">
-                <img src={star} className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
-            </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">From {props.price}</span> / person</p>
-        </div>
-    )
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img
+        src={`/src/assets/${props.img}`}
+        className="card--image"
+        alt={props.alt}
+      />
+      <div className="card--stats">
+        <img src={star} className="card--star" />
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
+      </div>
+      <p className="card--title">{props.title}</p>
+      <p className="card--price">
+        <span className="bold">From {props.price}</span> / person
+      </p>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
