7826a5cdffaa3243c9f4107af323fb4aa1436064
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-16 10:19:31 -0300

    Map experiences data into Card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Card from './components/Card'
-import data from './data'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
 
 function App() {
+  const cards = data.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        img={card.coverImg}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        location={card.location}
+        title={card.title}
+        price={card.price}
+      />
+    );
+  });
+
   return (
     <>
       <Navbar />
       <Hero />
-      <Card 
-        img="katie-zaferes.png"
-        alt="Katie Zaferes swimming"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
